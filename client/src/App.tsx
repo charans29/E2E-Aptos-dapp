b0864745fe6ec6a18f38495baaa187399fb3f4da
charans29 <smodugula@csuchico.edu>
2024-04-07 17:34:04 -0700

    Implemented Ant UI

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import { Layout, Row, Col } from "antd";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <h1>Connect Wallet</h1>
+          </Col>
+        </Row>
+      </Layout>
+    </>
+  );
+}
+
+
+
+export default App;
